1e127419f9fa847ebf11840d392798f7216cc590
Petar Vujovic <petarvujovic98@gmail.com>
2024-10-28 12:11:47 +0100

    debug(runtime): debug error type

--- program-runtime/src/invoke_context.rs
+++ program-runtime/src/invoke_context.rs
@@ -533,6 +533,7 @@ impl<'a> InvokeContext<'a> {
                 Ok(())
             }
             ProgramResult::Err(ref err) => {
+                println!("{err:?}");
                 if let EbpfError::SyscallError(syscall_error) = err {
                     if let Some(instruction_err) = syscall_error.downcast_ref::<InstructionError>()
                     {
