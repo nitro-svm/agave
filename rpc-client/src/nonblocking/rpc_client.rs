bcf10424f3896fc978ad33d0b3207549a348cba5
Petar Vujović <petarvujovic98@gmail.com>
2025-01-15 09:32:00 +0100

    chore: Remove `Base64Zstd` usage from `rpc-client` (#17)

--- rpc-client/src/nonblocking/rpc_client.rs
+++ rpc-client/src/nonblocking/rpc_client.rs
@@ -3417,7 +3417,8 @@ impl RpcClient {
         commitment_config: CommitmentConfig,
     ) -> RpcResult<Option<Account>> {
         let config = RpcAccountInfoConfig {
-            encoding: Some(UiAccountEncoding::Base64Zstd),
+            // encoding: Some(UiAccountEncoding::Base64Zstd), Zstd is not supported by svm-rollup
+            encoding: Some(UiAccountEncoding::Base64),
             commitment: Some(commitment_config),
             data_slice: None,
             min_context_slot: None,
@@ -3644,7 +3645,8 @@ impl RpcClient {
         self.get_multiple_accounts_with_config(
             pubkeys,
             RpcAccountInfoConfig {
-                encoding: Some(UiAccountEncoding::Base64Zstd),
+                // encoding: Some(UiAccountEncoding::Base64Zstd), Zstd is not supported by svm-rollup
+                encoding: Some(UiAccountEncoding::Base64),
                 commitment: Some(commitment_config),
                 data_slice: None,
                 min_context_slot: None,
@@ -3917,7 +3919,8 @@ impl RpcClient {
             pubkey,
             RpcProgramAccountsConfig {
                 account_config: RpcAccountInfoConfig {
-                    encoding: Some(UiAccountEncoding::Base64Zstd),
+                    // encoding: Some(UiAccountEncoding::Base64Zstd), Zstd is not supported by svm-rollup
+                    encoding: Some(UiAccountEncoding::Base64),
                     ..RpcAccountInfoConfig::default()
                 },
                 ..RpcProgramAccountsConfig::default()
