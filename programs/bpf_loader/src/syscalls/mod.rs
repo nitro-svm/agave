27b2f3fe3b4ae4f1de3e312460d0287c34976988
Jason Davis <jason@nitro.technology>
2025-08-07 10:30:13 -0500

    fix: Update memory-map of an AccountInfo struct to a VmAccountInfo (#24)
    * fix: compilation fixes after Agave update-and-merge (#23)
    
    * Compilation fixes after Agave update-and-merge
    
    * Update core/Cargo.toml
    
    Co-authored-by: Petar Vujović <petarvujovic98@gmail.com>
    
    ---------
    
    Co-authored-by: Petar Vujović <petarvujovic98@gmail.com>
    
    * Update test code that maps a VmAccountInfo using raw memory
    
    * fix: compilation fixes after Agave update-and-merge (#23)
    
    * Compilation fixes after Agave update-and-merge
    
    * Update core/Cargo.toml
    
    Co-authored-by: Petar Vujović <petarvujovic98@gmail.com>
    
    ---------
    
    Co-authored-by: Petar Vujović <petarvujovic98@gmail.com>
    
    * Put Epoch back
    
    ---------
    
    Co-authored-by: Petar Vujović <petarvujovic98@gmail.com>

--- programs/bpf_loader/src/syscalls/mod.rs
+++ programs/bpf_loader/src/syscalls/mod.rs
@@ -293,6 +293,15 @@ pub struct VmNonNull<T> {
     resource_type: PhantomData<T>,
 }
 
+impl<T> VmNonNull<T> {
+    pub fn from_addr(addr: u64) -> Self {
+        Self {
+            addr,
+            resource_type: PhantomData,
+        }
+    }
+}
+
 #[derive(Clone)]
 #[repr(C)]
 pub struct VmBoxOfRefCell<T> {
@@ -300,6 +309,17 @@ pub struct VmBoxOfRefCell<T> {
     _weak_addr: u64,
     _borrow_flag: u64,
     pub value: T,
+}
+
+impl<T> VmBoxOfRefCell<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            _strong_addr: 0,
+            _weak_addr: 0,
+            _borrow_flag: 0,
+            value,
+        }
+    }
 }
 
 /// Account information, in the virtual address space. Note: Since the addresses are u64,
@@ -2184,7 +2204,6 @@ declare_builtin_function!(
 #[allow(clippy::arithmetic_side_effects)]
 #[allow(clippy::indexing_slicing)]
 mod tests {
-    use solana_account_info::AccountInfo;
     #[allow(deprecated)]
     use solana_sysvar::fees::Fees;
     use {
@@ -4470,7 +4489,7 @@ mod tests {
             META_OFFSET + std::mem::size_of::<ProcessedSiblingInstruction>();
         const DATA_OFFSET: usize = PROGRAM_ID_OFFSET + std::mem::size_of::<Pubkey>();
         const ACCOUNTS_OFFSET: usize = DATA_OFFSET + 0x100;
-        const END_OFFSET: usize = ACCOUNTS_OFFSET + std::mem::size_of::<AccountInfo>() * 4;
+        const END_OFFSET: usize = ACCOUNTS_OFFSET + std::mem::size_of::<VmAccountInfo>() * 4;
         let mut memory = [0u8; END_OFFSET];
         let config = Config::default();
         let mut memory_mapping = MemoryMapping::new(
