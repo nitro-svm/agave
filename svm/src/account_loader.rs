915d59845337ef653cede2b37edc98bd3918f747
Petar Vujovic <petarvujovic98@gmail.com>
2025-10-06 21:32:43 +0200

    dbg

--- svm/src/account_loader.rs
+++ svm/src/account_loader.rs
@@ -666,6 +666,7 @@ fn load_transaction_accounts_simd186<CB: TransactionProcessingCallback>(
     for (program_id, instruction) in message.program_instructions_iter() {
         let Some(program_account) = account_loader.load_account(program_id) else {
             error_metrics.account_not_found += 1;
+            log::debug!("Program account not found: {program_id}");
             return Err(TransactionError::ProgramAccountNotFound);
         };
 
