b8fe3a4a7fb71063fa73f970e47e1c0855d18ca6
Petar Vujovic <petarvujovic98@gmail.com>
2025-10-06 21:19:03 +0200

    dbg

--- svm/src/program_loader.rs
+++ svm/src/program_loader.rs
@@ -217,6 +217,7 @@ pub(crate) fn get_program_modification_slot<CB: TransactionProcessingCallback>(
     callbacks: &CB,
     pubkey: &Pubkey,
 ) -> TransactionResult<Slot> {
+    log::debug!("Getting program modification slot for {pubkey}");
     let program = callbacks
         .get_account_shared_data(pubkey)
         .ok_or(TransactionError::ProgramAccountNotFound)?;
