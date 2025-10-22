a4c38f175c1434c12c20b95dd843b791f0aba505
Petar Vujovic <petarvujovic98@gmail.com>
2025-10-22 17:42:55 +0200

    chore: Add log to program not found

--- svm/src/account_loader.rs
+++ svm/src/account_loader.rs
@@ -616,7 +616,7 @@ fn load_transaction_accounts_simd186<CB: TransactionProcessingCallback>(
     for (program_id, instruction) in message.program_instructions_iter() {
         let Some(program_account) = account_loader.load_account(program_id) else {
             error_metrics.account_not_found += 1;
-            println!("Program account not found: {program_id}");
+            log::error!("Program account not found: {program_id}");
             return Err(TransactionError::ProgramAccountNotFound);
         };
 
@@ -688,7 +688,7 @@ fn load_transaction_accounts_old<CB: TransactionProcessingCallback>(
 
             let Some(program_account) = account_loader.load_account(program_id) else {
                 error_metrics.account_not_found += 1;
-                println!("Program account not found old: {program_id}");
+                log::error!("Program account not found old: {program_id}");
                 return Err(TransactionError::ProgramAccountNotFound);
             };
 
@@ -712,7 +712,7 @@ fn load_transaction_accounts_old<CB: TransactionProcessingCallback>(
                     validated_loaders.insert(*owner_id);
                 } else {
                     error_metrics.account_not_found += 1;
-                    println!("Program account owner not found: {owner_id}");
+                    log::error!("Program account owner not found: {owner_id}");
                     return Err(TransactionError::ProgramAccountNotFound);
                 }
             }
